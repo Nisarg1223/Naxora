a44bc0b67dc4f233fa9e5d72a5760af969add16b
Nisarg1223 <darjinisarg49@gmail.com>
2026-06-22 15:10:26 +0530

    loading animation added

--- Perplexity/Frontend/src/app/features/auth/components/Protected.jsx
+++ Perplexity/Frontend/src/app/features/auth/components/Protected.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router';
+import './Protected.scss';
 
 const Protected = ({children}) => {
     const user = useSelector(state => state.auth.user);
     const loading = useSelector(state => state.auth.loading);
 
-
     if(loading){
-        return <div>Loading.....</div>
+        return (
+            <div className="auth-loading-screen">
+                <div className="loading-content">
+                    <div className="loading-logo">
+                        {"NEXORA".split("").map((letter, index) => (
+                            <span key={index} style={{ animationDelay: `${index * 0.08}s` }}>
+                                {letter}
+                            </span>
+                        ))}
+                    </div>
+                    <div className="loading-copy">
+                        Loading
+                        <span className="loading-dots">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
     }
+
     if(!user){
         return <Navigate to="/login" replace/>
     }
-  
 
   return children;
 }
